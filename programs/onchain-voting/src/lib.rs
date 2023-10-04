94e0aa53fada7233b8680833e5992b7a6aeded63
Dean 利迪恩 <10921578+deanmlittle@users.noreply.github.com>
2023-10-05 07:47:45 +0800

    Update lib.rs

--- programs/onchain-voting/src/lib.rs
+++ programs/onchain-voting/src/lib.rs
@@ -10,15 +10,23 @@ pub mod onchain_voting {
     use super::*;
 
     // Function to initialize the vote bank
-    pub fn init_vote_bank(ctx: Context<InitVote>) -> Result<()> {
-        // Set the vote account to be open for public voting
-        ctx.accounts.vote_account.is_open_to_vote = true;
+    pub fn init_vote_bank(ctx: Context<InitVote>, prompt: String, seed: u64) -> Result<()> {
+        // Set the seed
+        ctx.accounts.vote_account.seed = seed;
+        // Set the bump
+        ctx.accounts.vote_account.bump = ctx.bumps.get("vote_account").unwrap();
+        // Set the expiration of the vote to 100,000 slots from now
+        ctx.accounts.vote_account.expiration = Clock::get()?.slot + 100_000;
+        // Set the prompt being voted on
+        ctx.accounts.vote_account.prompt = prompt;
         // Return an empty result
         Ok(())
     }
 
     // Function to handle a user's vote
     pub fn gib_vote(ctx: Context<GibVote>, vote_type: VoteType) -> Result<()> {
+        // Make sure voting isn't expired yet
+        require!(ctx.accounts.vote_account.expiration > Clock::get()?.slot);
         // Match the vote type (GM or GN) and increment the corresponding counter
         match vote_type {
             VoteType::GM => {
@@ -41,13 +49,16 @@ pub mod onchain_voting {
 
 // Define the accounts struct for initializing the vote bank
 #[derive(Accounts)]
+#[instruction(prompt: String, seed: u64)]
 pub struct InitVote<'info> {
     // Define the vote account with the necessary attributes
     #[account(
         init, 
-        payer = signer, 
-        space = 8 + 1 + 8 + 8, 
-    )] 
+        seeds = [b"proposal", seed.to_le_bytes().as_ref()],
+        bump,
+        payer = signer,
+        space = 8 + 8 + 1 + 8 + 8 + 8 + 4 + prompt.len(),
+    )]
     pub vote_account: Account<'info, VoteBank>,
     // Define the signer account with mutable attribute
     #[account(mut)]
@@ -59,19 +70,29 @@ pub struct InitVote<'info> {
 // Define the VoteBank account struct
 #[account]
 pub struct VoteBank {
-    // Flag to indicate if the account is open for voting
-    pub is_open_to_vote: bool,
+    // Seed
+    pub seed: u64
+    // Bump
+    pub bump: u8,
     // Counter for GM votes
     pub gm: u64,
     // Counter for GN votes
     pub gn: u64,
+    // Expiration (u64 for slots, i64 for ms)
+    pub expiration: u64,
+    // The prompt to be voted on
+    pub prompt: String,
 }
 
 // Define the accounts struct for handling a user's vote
 #[derive(Accounts)]
 pub struct GibVote<'info> {
     // Define the mutable vote account
-    #[account(mut)] 
+    #[account(
+        mut,
+        seeds = [b"proposal", vote_account.seed.to_le_bytes().as_ref()],
+        bump = vote_account.bump,
+    )]
     pub vote_account: Account<'info, VoteBank>,
     // Define the signer account
     pub signer: Signer<'info>,
